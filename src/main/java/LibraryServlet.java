0b3c999dbfd50047bc284dcc304556eb5cf881b6
loudsurfer <loudsurfer@gmail.com>
2021-06-14 22:25:07 +0300

    пример подключения к базе данных с использованием jdbc

---
+++ src/main/java/LibraryServlet.java
@@ -0,0 +1,40 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet(name = "LibraryServlet", value = "/LibraryServlet")
+public class LibraryServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/java_ee_db",
+                    "user", "");
+            Statement stmt = connection.createStatement();
+            ResultSet resultSet = stmt.executeQuery("SELECT title from books");
+
+            while (resultSet.next()){
+                pw.println(resultSet.getString("title"));
+            }
+
+            stmt.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
